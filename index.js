981c27c471ee79b459cc7a819e27ee7e0fd27e0a
Abhishek Jain <abhishekjain905824@gmail.com>
2026-09-11 19:45:19 +0530

    module connect

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-require('dotenv').config()
+import express from 'express'
+import 'dotenv/config'
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
 
 app.get('/myname', (req, res) => {
   res.send('<h1>Abhishek Jain! </h1>')
+})
+
+app.get('/jokes', (req , res) =>{
+  const jokes = [{
+    id: 1,
+    joke : "first joke"
+  },
+  {
+    id : 2,
+    joke : "second joke"
+  }]
+  res.send(jokes)
 })
 
 app.listen(process.env.PORT, () => {
